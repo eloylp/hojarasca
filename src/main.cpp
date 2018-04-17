c345db2618f07ccfa4d0da33a315f3b06bda78bd
eloylp <e.lopez@xeitodevs.com>
2018-04-17 23:58:27 +0200

    Fixing memory leak. And using std better.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <bits/unique_ptr.h>
 #include "RelativeClock.h"
 
 int main(int argc, char *argv[]) {
 
-    auto *relativeClock = new RelativeClock();
+    std::unique_ptr<RelativeClock> relativeClock(new RelativeClock);
     relativeClock->start();
-    std::cout << "\n";
+    std::cout << std::endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
